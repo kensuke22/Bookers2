47cde23af13c06fe849c9cc1a5d630afe4ecd08c
kensuke22 <b430022222222@gmail.com>
2021-05-14 10:48:21 +0000

    [add]4-10  Bootstrap

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root to: 'homes#top'
   devise_for :users
-  resources :users, only: [:new, :create, :index, :show, :destroy]
+  resources :users
+  resources :books
   
 end
